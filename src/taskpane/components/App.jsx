776ab63a4c968122b15f3cda827eb381d879cc6e
Murray Toews <mstoews@hotmail.com>
2024-01-09 00:36:27 +0900

    initial update

---
+++ src/taskpane/components/App.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Header from "./Header";
+import TextInsertion from "./TextInsertion";
+import { makeStyles } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+  },
+});
+
+const App = (props) => {
+  const styles = useStyles();
+  // The list items are static and won't change at runtime,
+  // so this should be an ordinary const, not a part of state.
+  return (
+    <div className={styles.root}>
+      <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
+      <TextInsertion />
+    </div>
+  );
+};
+
+App.propTypes = {
+  title: PropTypes.string,
+};
+
+export default App;
